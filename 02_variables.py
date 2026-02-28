00d5a08e16a3e38cbaa2b3af6dd6fecba2ae452b
vijay M <viji84484@gmail.com>
2026-02-28 23:58:27 +0530

    first commit

---
+++ 02_variables.py
@@ -0,0 +1,53 @@
+"""
+Python Variables Examples.
+*Python is dynamically typed
+  -while creating variables no need to mention data types.
+  
+*Variable stores reference to object
+* Value can be changed
+
+"""
+'''Python Variables Examples.'''
+# Assigning values to variables
+
+name = "John"
+age = 22
+salary = 25000.50
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Is Student:", is_student)
+
+
+#Reassigning Variable
+
+a = 10
+print("Initial value:", a)
+
+a = 20
+print("Updated value:", a)
+
+
+#Multiple Assignment
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+#Can we change variable type
+
+A = 10
+A = "Python"
+print(A)
+print(type(A))
+
+
+
+
+
+
+
+
+
+
---
+++ 03_Identifiers.py
@@ -0,0 +1,28 @@
+
+'''
+Identifier is the name given to Variable, Function, Class
+
+
+    # Identifiers Rules:
+    ---------------------
+
+   - we should not use keyword as indentifier
+   - as a first charecter of indentifier should not use number
+   - ather than underscore we cannot use any special charecter
+   - we can use combination of alphabets, numbers and underscore
+   - maximum length of identifier is 79 charecter 
+
+'''
+
+
+valid_name = "Python"
+_valid = 100
+name1 = "Valid"
+nam_12_e="vijay"
+
+# Invalid examples (commented because they cause error)
+
+# 1name = "Error"     # Cannot start with number
+# class = "Error"     # Cannot use keyword
+
+print("Valid identifiers created successfully.")
